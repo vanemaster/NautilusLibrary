3d5a842eefb141945b024108f1b45af43b06e1d8
Vanessa França <vafranca@gmail.com>
2021-06-29 21:33:11 -0300

    corrigir bug misterioso

--- app/src/main/java/com/example/nautiluslibrary/LivroActivity.java
+++ app/src/main/java/com/example/nautiluslibrary/LivroActivity.java
@@ -139,7 +139,7 @@ public class LivroActivity extends AppCompatActivity {
     }
     public byte[] getBytesFromBitmap(Bitmap bitmap) {
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 70, stream);
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream);
         return stream.toByteArray();
     }
 
